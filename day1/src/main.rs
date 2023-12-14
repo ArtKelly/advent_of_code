70f16a5e3c19f0b3bcff2b8bbb1863839e002dc7
Art <art@riptidewave.com>
2023-12-14 19:30:18 +0000

    Generate day1 from template

---
+++ day1/src/main.rs
@@ -0,0 +1,32 @@
+use aocd::*;
+
+fn part_a(_input: String) -> usize {
+    0
+}
+
+fn part_b(_input: String) -> usize {
+    0
+}
+
+#[aocd(2023, 1)]
+fn main() {
+    submit!(1, part_a(input!()));
+    submit!(2, part_b(input!()));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_a() {
+        let result = part_a(include_str!("test.txt").to_string());
+        assert_eq!(result, 0);
+    }
+
+    #[test]
+    fn test_part_b() {
+        let result = part_b(include_str!("test.txt").to_string());
+        assert_eq!(result, 0);
+    }
+}
